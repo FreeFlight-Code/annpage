6824df3db3d197d75558739f2ab166c046230362
David Fischer <david@simustream.com>
2019-04-01 21:27:50 -0600

    Updates

---
+++ precache-manifest.dc675c93be162ee0c4161a9fe76bd792.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c995dc2877fc8b27fd4bcc1db1895a8",
+    "url": "/annpage/static/media/scotland.4c995dc2.jpg"
+  },
+  {
+    "revision": "9a5b61a7d904deec7cd48f4da03b2de5",
+    "url": "/annpage/static/media/annette_hat.9a5b61a7.jpg"
+  },
+  {
+    "revision": "92a0280edee59ea9e122bf5553d89dae",
+    "url": "/annpage/static/media/annette_dark.92a0280e.jpg"
+  },
+  {
+    "revision": "deec9ce4f54fd4ddf004",
+    "url": "/annpage/static/js/runtime~main.280fced8.js"
+  },
+  {
+    "revision": "2d84fa01d08cb63ff926",
+    "url": "/annpage/static/js/main.0d726f43.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/annpage/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "2d84fa01d08cb63ff926",
+    "url": "/annpage/static/css/main.7e612d12.chunk.css"
+  },
+  {
+    "revision": "1fc73e25345342966af3119e9b7b5959",
+    "url": "/annpage/index.html"
+  }
+];
