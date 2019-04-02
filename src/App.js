2cbe973e525b2a37410d65049b5210bd7cf437ff
David Fischer <david@simustream.com>
2019-04-02 10:04:17 -0600

    added social media

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import './App.scss';
 import annette from '../src/images/annette_hat.jpg';
+import facebook from '../src/images/facebook.png';
+import instagram from '../src/images/instagram.png';
+import twitter from '../src/images/twitter.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className='header'>
-          <div className='social'>social media icons</div>
-          <div className='nav'>navigation bar or welcome banner</div>
+          <div className='social'>
+            <span className='left'>
+                <span>1-800-234-5678</span>
+                <img src={facebook} className='icon facebook'></img>
+                <img src={instagram} className='icon instagram'></img>
+                <img src={twitter} className='icon twitter'></img>
+            </span>
+            <span className='right'>LOGIN</span>
+          </div>
+          <div className='nav'>ANNETTE SHAW</div>
         </header>
         <div className='mainImg'>
           <div id='mainImgContainer'></div>
           <div className='text'>
             <h1>TRANSFORM YOUR LIFE</h1>
-            <h4>CLOSE THE GAP BETWEEN WHERE YOU ARE TODAY - AND WHERE YOU WANT TO BE</h4>
+            <h5>CLOSE THE GAP BETWEEN WHERE YOU ARE TODAY - AND WHERE YOU WANT TO BE</h5>
             <button>Take our free Life Assessment</button>
           </div>
         </div>
@@ -25,14 +36,14 @@ class App extends Component {
         <div className='direction'>
           <div className='cardContainer one'>
           <div className='cardImage'></div>
-            <h4>DO YOU WANT TO IMPROVE YOUR BUSINESS</h4>
-            <h6>Get the insight, strategies, and tools you need to get the edge over your competition and drive your business to the next level.</h6>
+            <h4>HAVE YOU BEEN THROUGH TRAUMA?</h4>
+            <h6>Have you known trauma and abuse?  Are you still standing but need help getting past blockages that are preventing you from being your authentic self?</h6>
             <button className='clear'>LEARN MORE</button>
           </div>
           <div className='cardContainer two'>
             <div className='cardImage'></div>
-            <h4>{'Do you want to Improve your personal life?'.toUpperCase()}</h4>
-            <h6>Are you successful and truly happy, or are there parts of you that are unfulfilled? The extraordinary life you want is within your grasp, but you need clarity and focus to achieve it..</h6>
+            <h4>ARE YOU ATTRACTING THE WRONG MAN?</h4>
+            <h6>Where is your God man?  Do you want to foster a man's desire to care and protect you?  Are you trying to save a relationship that feels like it is lost?</h6>
             <button className='clear'>LEARN MORE</button>
           </div>
         </div>
@@ -45,9 +56,12 @@ class App extends Component {
             <p className='clickable'>Read more...</p>
           </div>
         </div>
-        {/* <div className='testimonials'>
-          Testimonials
-        </div> */}
+        <div className='subscribe'>
+          <div>Subscribe to Annette's Newsletter</div>
+          <div>Get the latest news and stories delivered to your inbox.</div>
+          <button>SIGN UP NOW</button>
+        </div>
+        <footer>{'All Rights Reserved 2019 Annette Shaw' + String.fromCharCode(169)}</footer>
       </div>
     );
   }
