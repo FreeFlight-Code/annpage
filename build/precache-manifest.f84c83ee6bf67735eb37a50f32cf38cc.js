bcafc92b585452a05c72f98b5be0790b646a2c61
David Fischer <david@simustream.com>
2019-10-21 11:48:40 -0600

    build added

---
+++ build/precache-manifest.f84c83ee6bf67735eb37a50f32cf38cc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "34531bb4e81cff3cefa2586b878b14e3",
+    "url": "/ars/static/media/facebook.34531bb4.png"
+  },
+  {
+    "revision": "c9bd324b6c3d5234129e",
+    "url": "/ars/static/css/main.a4ab266a.chunk.css"
+  },
+  {
+    "revision": "5bcb8511d54224b339a3",
+    "url": "/ars/static/js/runtime~main.6e6b591b.js"
+  },
+  {
+    "revision": "8ac306522c96742c9819",
+    "url": "/ars/static/js/2.ec93214b.chunk.js"
+  },
+  {
+    "revision": "9a5b61a7d904deec7cd48f4da03b2de5",
+    "url": "/ars/static/media/annette_hat.9a5b61a7.jpg"
+  },
+  {
+    "revision": "c9bd324b6c3d5234129e",
+    "url": "/ars/static/js/main.5708ae4e.chunk.js"
+  },
+  {
+    "revision": "5dc9113606d6a04595b9e964fc4d368f",
+    "url": "/ars/static/media/twitter.5dc91136.png"
+  },
+  {
+    "revision": "92a0280edee59ea9e122bf5553d89dae",
+    "url": "/ars/static/media/annette_dark.92a0280e.jpg"
+  },
+  {
+    "revision": "4c995dc2877fc8b27fd4bcc1db1895a8",
+    "url": "/ars/static/media/scotland.4c995dc2.jpg"
+  },
+  {
+    "revision": "0b8407744dab527ca6d438f06265b345",
+    "url": "/ars/static/media/ptsdwords.0b840774.jpg"
+  },
+  {
+    "revision": "24b49a87c051254c57450d0ebc86b5f9",
+    "url": "/ars/index.html"
+  }
+];
